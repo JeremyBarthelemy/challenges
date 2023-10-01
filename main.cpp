b2c61071627007d35d6d8436d88dbe5d5be2611a
Jeremy Barthelemy <j.l.barthel@gmail.com>
2023-10-01 07:11:30 -0700

    file args

--- main.cpp
+++ main.cpp
@@ -3,9 +3,24 @@
 
 
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "hello!" << std::endl;
+
+
+    if (argc == 1)
+    {
+        std::cout << "Just one file." << std::endl;
+
+    }
+    else
+    {
+        for (int i = 1; i < argc; ++i)
+        {
+
+            std::cout << "additional file: " << i << std::endl;
+        }
+    }
 
     return 0;
 }
